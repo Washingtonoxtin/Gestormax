be4d412baa208d408427c7d92f015c7e504b029d
washingtonoxtin <washingtonprograma@gmail.com>
2025-06-07 14:50:35 -0300

    V2

--- src/main/java/com/faculdade/gestormax/config/CorsConfig.java
+++ src/main/java/com/faculdade/gestormax/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Aplica a todos os endpoints da sua API
-                        .allowedOrigins("http://localhost:8081", "http://192.168.0.6:8081") // Origens permitidas
+                        .allowedOrigins("http://localhost:8081", "http://10.0.022:8081") // Origens permitidas
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
                         .allowedHeaders("*") // Permite todos os cabeçalhos
                         .allowCredentials(true) // Permite o envio de credenciais (cookies, etc.)
